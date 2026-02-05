12de601d290904b6215dc64f33278f4600b7c35f
Marion Boulin <marion.boulin@imt-atlantique.net>
2026-02-05 20:38:55 +0100

    Delete Projets_Finance/autocall_pricer directory

--- Projets_Finance/autocall_pricer/autocall_pricer/pricing.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def price_product(payoffs, r, T):
-    return np.mean(payoffs) * np.exp(-r * T) # On actualise la moyenne des payoffs à la valeur présente en utilisant le taux d'actualisation r et la maturité T
--- Projets_Finance/autocall_pricer/pricing.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def price_product(payoffs, r, T):
-    return np.mean(payoffs) * np.exp(-r * T)
