dfb592f4795996aa6075b6888c66faa92172aaf1
Marion Boulin <marion.boulin@imt-atlantique.net>
2026-02-05 20:34:32 +0100

    Add comments to main.py for clarity
    Added comments in French to clarify the simulation and pricing process.

--- Projets_Finance/autocall_pricer/main.py
+++ Projets_Finance/autocall_pricer/main.py
@@ -12,12 +12,13 @@ params = {
     "barriere_pdi": p.barriere_pdi
 }
 
+# simuler les trajectoires de l'indice sous-jacent ( mettre grand nombre de simulations mm si on affiche qu'une partie des trajectoires)
 paths = simulate_paths(
     p.S0, p.r, p.sigma, p.T, p.n_steps, p.n_simulations
 )
 
-payoffs = autocall_payoff(paths, params)
-price = price_product(payoffs, p.r, p.T)
+payoffs = autocall_payoff(paths, params) # On calcule les payoffs à partir des trajectoires simulées et des paramètres du produit
+price = price_product(payoffs, p.r, p.T) # c'est le prix du produit, c'est à dire la valeur actuelle des payoffs moyens obtenus à partir de la simulation
 
 print(f"Prix du produit : {price:.2f}")
 plot_paths(paths)
