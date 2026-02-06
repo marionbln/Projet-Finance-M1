ec35e339561043b5410999b855feea53ce20480a
Marion Boulin <marion.boulin@imt-atlantique.net>
2026-02-06 14:40:54 +0100

    Enhance comments for barrier parameters in parameters.py
    Added explanations for barrier parameters in product settings.

--- Projets_Finance/autocall_pricer/parameters.py
+++ Projets_Finance/autocall_pricer/parameters.py
@@ -8,11 +8,12 @@ T = 6             # Maturité (années)
 
 # Paramètres produit
 coupon = 0.06     # 6% par an
-barriere_rappel = 1.0     # 100%
-barriere_pdi = 0.6        # 60%
+barriere_rappel = 1.0     # 100%  elle declenche remboursement anticipé du produit à date d'anniv, si l'indice S au dessus de Pdi alors investisseur recoit capital + nb d'années écoulées
+barriere_pdi = 0.6        # 60% declenche protection capital à 100% à l'echeance (put down andin)
 nominal = 100
 
 # Monte-Carlo
 n_simulations = 50_000
 n_steps = T      # 1 observation par an
 dt = T / n_steps
+
